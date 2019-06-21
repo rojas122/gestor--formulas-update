3fa3fbff72c93c409d3f6d941eea716cfb7270c7
ParzivalWatts <gcpaulo@unicauca.edu.co>
2019-06-21 08:40:06 -0500

    merged and branch

--- app/src/main/java/com/aplimovil/formulas/Main_y_category/MainActivity.java
+++ app/src/main/java/com/aplimovil/formulas/Main_y_category/MainActivity.java
@@ -35,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn_codg_qr:
                 intent=new Intent(MainActivity.this, escanner.class);
                 break;
-            case R.id.favoritas:
+            /*case R.id.favoritas:
                 intent=new Intent(MainActivity.this, FAVORITOS.class);
-                break;
+                break;}*/
             case R.id.btn_favoritas2:
                 intent=new Intent(MainActivity.this, FAVORITOS.class);
                 break;
